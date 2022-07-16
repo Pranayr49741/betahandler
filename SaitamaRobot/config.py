4082fe37256aa0878470c3eeb404f8b984c5fec2
Pranayr49741 <96970138+Pranayr49741@users.noreply.github.com>
2022-07-16 23:51:42 +0530

    Update config.py

--- SaitamaRobot/config.py
+++ SaitamaRobot/config.py
@@ -5,7 +5,7 @@ import os
 
 def get_user_list(config, key):
     with open("SaitamaRobot/__init__.py".format(os.getcwd(), config), "r") as json_file:
-        return json.load(json_file)[key]
+        return json.load(json_file)
 
 
 # Create a new config.py or rename this to config.py file in same dir and import, then extend this class.
